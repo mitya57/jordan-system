mitya57/jordan-system

commit c0c3fdba137f43f2d49b15dde5600cd210304377

Speed up by using matrix coordinates when finding main block

mpi-util.hh
@@ -287,20 +287,16 @@ void mpi_read_matrix(MPI_Data *data, char *filename) {
 
 void mpi_find_and_move_main_block(MPI_Data *data, int s) {
 	MPI_Double_Int sendbuf, recvbuf;
-	int localx, localy, matrixx, matrixy;
+	int matrixx, matrixy;
 	double norm;
 	sendbuf.value = -1;
 	sendbuf.pos = -1;
 	// find the main block
-	for (localx = 0; (localx+1)*BLOCKSIZE <= ROW_HEIGHT_FOR_P(data->rank); ++localx)
-		for (localy = 0; (localy+1)*BLOCKSIZE <= SIZE; ++localy) {
+	for (matrixx = s; (matrixx+1)*BLOCKSIZE <= ROW_HEIGHT_FOR_P(data->rank); ++matrixx)
+		for (matrixy = s; (matrixy+1)*BLOCKSIZE <= SIZE; ++matrixy) {
 			// calculate norm
-			matrixx = mpi_localx_to_matrixx(data, localx);
-			matrixy = mpi_localy_to_matrixy(data, localy);
-			if (matrixx < s || matrixy < s)
-				continue;
 			if (!block_get_reverse(BLOCKSIZE,
-			mpi_get_local_block(data, localx, localy),
+			mpi_get_pos_block(data, matrixx, matrixy),
 			data->blockbuf, data->tempbuf))
 				continue;
 			norm = block_get_norm(BLOCKSIZE, data->blockbuf);

commit 282dec551558c44527e540da67183ed070d6c18f

Do not check norm for non-local blocks (fixes a regression from c0c3fdba13)

mpi-util.hh
@@ -291,6 +291,8 @@ void mpi_find_and_move_main_block(MPI_Data *data, int s) {
 	// find the main block
 	for (matrixx = s; (matrixx+1)*BLOCKSIZE <= ROW_HEIGHT_FOR_P(data->rank); ++matrixx)
 		for (matrixy = s; (matrixy+1)*BLOCKSIZE <= SIZE; ++matrixy) {
+			if (data->rank != mpi_rank_for_x(data, matrixx))
+				continue;
 			// calculate norm
 			if (!block_get_reverse(BLOCKSIZE,
 			mpi_get_pos_block(data, matrixx, matrixy),